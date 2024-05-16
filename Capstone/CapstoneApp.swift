2aff444b9c4b1bd61feff51e8af23c782840fb13
evgenii <evgenii@calthings.com>
2024-05-16 08:02:55 -0700

    Progect setup

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,20 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Evgenii Iavorovich on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
